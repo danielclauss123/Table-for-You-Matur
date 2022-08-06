a504e75b13835fa017d57cf0a372724c7d7e8d1b
Daniel Clauss <danielclauss@gmx.ch>
2022-08-06 15:21:06 +0200

    Initial Commit

---
+++ Table for You/Table_for_YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Table_for_YouApp.swift
+//  Table for You
+//
+//  Created by Daniel Clauss on 06.08.22.
+//
+
+import SwiftUI
+
+@main
+struct Table_for_YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
