5f4adb279d44e748b60f708795cc1eeca0f9a8d0
Daniel Clauss <danielclauss@gmx.ch>
2022-11-10 18:46:04 +0100

    Initial Commit

---
+++ Table for You Matur/Table_for_You_MaturApp.swift
@@ -0,0 +1,17 @@
+//
+//  Table_for_You_MaturApp.swift
+//  Table for You Matur
+//
+//  Created by Daniel Clauss on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Table_for_You_MaturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
